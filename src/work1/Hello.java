3fa2b1f151684444cf153fa2792cbd677b36795c
msahin <mrtsahin79@gmail.com>
2022-02-12 16:10:12 -0500

    2nd commit

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("work1");
+        System.out.println("work1");
         System.out.println("work2");
     }
 }
